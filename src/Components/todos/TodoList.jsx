76b3405613e20ec4cc6adf8cca3cd195fc35c5e6
aliehs74 <fe.afra@gmail.com>
2023-12-23 21:55:47 +0330

    finilize todolist template

--- src/Components/todos/TodoList.jsx
+++ src/Components/todos/TodoList.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 const TodoList = () => {
   const todos = useSelector((state) => state.todo.data);
   const todosId = Object.keys(todos);
+  console.log(todos[1]);
 
   const renderedListItems = todosId.map((id) => {
     return <TodoListItem key={id} todo={todos[id]} />;
--- src/Components/todos/TodoListItem.jsx
+++ src/Components/todos/TodoListItem.jsx
@@ -1,10 +1,7 @@
+import { availableColors, capitalize } from "./colorProcess";
 import { ReactComponent as TimesSolid } from "./times-solid.svg";
 
-export const availableColors = ["green", "blue", "orange", "purple", "red"];
-export const capitalize = (s) => s[0].toUpperCase() + s.slice(1);
-
 const TodoListItem = ({ todo }) => {
-  console.log(todo);
   const { text, completed, color } = todo;
   const colorOptions = availableColors.map((c) => (
     <option key={c} value={c}>
